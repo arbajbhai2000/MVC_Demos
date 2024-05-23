002c045fa92d08e7ad18c5023ae90715e5823074
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-05-23 16:59:12 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using _2_ViewsInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_ViewsInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["name"] = "Tata Consaltancy Services";
+            ViewBag.Email = "support@tcs.com";
+
+            TempData["Address"] = "Value stored in tempdata";
+
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+             
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+             int number = 50;
+            return View("Index3" , number);
+        }
+
+        public ActionResult Index4()
+        {
+            Product p = new Product()
+            { Id = 1, Name = "Shirt", Price = 499 };
+            return View(p);
+        }
+
+        public ActionResult Index5()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 1,Name="Shirt" , Price=299},
+                new Product() { Id = 2, Name="Pant" , Price=399},
+                new Product() { Id = 3,Name="Tshirt" ,Price=499}
+            };
+            return View(products);
+        }
+
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using _2_ViewsInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_ViewsInMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 1,Name="Shirt" , Price=299},
+                new Product() { Id = 2, Name="Pant" , Price=399},
+                new Product() { Id = 3,Name="Tshirt" ,Price=499}
+            };
+            return View(products);
+             
+        }
+
+        public ActionResult Create() 
+        {
+            return View();
+        }
+
+
+        //public ActionResult Edit(int? id)
+        //{
+        //    List<Product> products = (List<Product>)TempData.Peek("products");
+
+        //    Product product = products.FirstOrDefault(p => p.Id == id);
+
+        //    return View(product);
+        //}
+
+        public ActionResult Edit(int? id)
+        {
+            if (!TempData.ContainsKey("products"))
+                return RedirectToAction("Index");
+
+            var products = TempData["products"] as List<Product>;
+            var product = products?.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return RedirectToAction("Index");
+
+            return View(product);
+        }
+
+    }
+}
