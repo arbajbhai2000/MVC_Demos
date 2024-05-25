00ba49744e7067f772bfc4e9288766d9709e503b
Shifa Mulani <shifamulani97911@gmail.com>
2024-05-25 16:50:21 +0530

    Area And Filter

---
+++ 4_AreaAndFilter/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using _4_AreaAndFilters.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4_AreaAndFilter.Controllers
+{
+    //[Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        //[RequireHttps]
+
+        [TraceRequestsAttribute]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+
+        public ActionResult PostComments(string comment)
+        {
+            ViewBag.Comment = comment;
+            return View("PostComments");
+        }
+
+        [OutputCache(Duration =15)]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            ViewBag.LoadedTime=DateTime.Now;
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Loginsection()
+        {
+            return PartialView("_LoginPartial");
+        }
+
+    }
+}
---
+++ 4_AreaAndFilter/Utility/TraceRequests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4_AreaAndFilters.Utility
+{
+    public class TraceRequestsAttribute : ActionFilterAttribute, IExceptionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string controller = filterContext.RouteData.Values["controller"].ToString();
+            string action = filterContext.RouteData.Values["action"].ToString();
+            string message = $"{controller} : {action} : OnActionExecuting : @{DateTime.Now.ToString()}\n";
+
+            LogMessageToFile(message);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string controller = filterContext.RouteData.Values["controller"].ToString();
+            string action = filterContext.RouteData.Values["action"].ToString();
+            string message = $"{controller} : {action} : OnActionExecuted : @{DateTime.Now.ToString()}\n";
+
+            LogMessageToFile(message);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            string controller = filterContext.RouteData.Values["controller"].ToString();
+            string action = filterContext.RouteData.Values["action"].ToString();
+            string message = $"{controller} : {action} : OnResultExecuting : @{DateTime.Now.ToString()}\n";
+
+            LogMessageToFile(message);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            string controller = filterContext.RouteData.Values["controller"].ToString();
+            string action = filterContext.RouteData.Values["action"].ToString();
+            string message = $"{controller} : {action} : OnResultExecuted : @{DateTime.Now.ToString()}\n";
+
+            LogMessageToFile(message);
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            string controller = filterContext.RouteData.Values["controller"].ToString();
+            string action = filterContext.RouteData.Values["action"].ToString();
+            string errorMessage = filterContext.Exception.Message;
+            string message = $"{controller} : {action} : OnException : " +
+                $"@{DateTime.Now.ToString()} Error Message : {errorMessage}\n";
+
+            LogMessageToFile(message);
+
+            filterContext.Result = new ViewResult()
+            {
+                ViewName = "Error"
+            };
+
+            filterContext.ExceptionHandled = true;
+        }
+
+        public void LogMessageToFile(string message)
+        {
+            string logFilePath = AppDomain.CurrentDomain.BaseDirectory + "/Logs/Logs.txt";
+            File.AppendAllText(logFilePath, message);
+        }
+    }
+}
