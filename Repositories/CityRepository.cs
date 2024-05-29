89c5e3717d1a8aa27bbcd782749cae7869f403f1
Arbajbhai2000 <arbajbhai2000@gmail.com>
2024-05-29 20:43:13 +0530

    repositories

---
+++ Repositories/CityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Data;
+using System.Collections.Generic;
+
+public class CityRepository : ICityRepository
+{
+    EmployeeDbContext _db = new EmployeeDbContext();
+
+
+    public bool Create(City city)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<City> GetAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<City> GetAllByDistrictId(int districtId)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Repositories/ICityRepository.cs
@@ -0,0 +1,11 @@
+﻿using Data;
+using System.Collections.Generic;
+
+public interface ICityRepository
+{
+    bool Create(City city);
+
+    List <City> GetAll();
+
+    List<City> GetAllByDistrictId(int districtId);
+}
